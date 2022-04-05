f47803955a2716168d9b2024f9953dd67ee57665
brunosa91 <brunosantana.engenharia@gmail.com>
2022-04-05 14:43:48 -0300

    estilizado a home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,18 @@
+import styles from "./Home.module.css";
+import savings from "../../img/savings.svg";
+import LinkButton from "../layout/LinkButton";
+
 function Home() {
-  return <h1>home</h1>;
+  return (
+    <section className={styles.home_container}>
+      <h1>
+        Bem-vindo a <span>Cost</span>
+      </h1>
+      <p>Comece a gerenciar os seus projetos agora mesmo</p>
+      <LinkButton to="/newproject" text="Criar projeto" />
+      <img src={savings} alt="costs" />
+    </section>
+  );
 }
 
 export default Home;
