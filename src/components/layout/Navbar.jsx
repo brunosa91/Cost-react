027d091335a666ae6e6425fd82712d892a621144
brunosa91 <brunosantana.engenharia@gmail.com>
2022-04-05 14:15:38 -0300

    criado o component navbar

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+import Container from "./Container";
+import styles from "./Navbar.module.css";
+import logo from "../../img/costs_logo.png";
+function NavBar() {
+  return (
+    <nav className={styles.navbar}>
+      <Container>
+        <Link to="/">
+          <img src={logo} alt="costs" />
+        </Link>
+
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/project">Projects</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/company">Company</Link>
+          </li>
+        </ul>
+      </Container>
+    </nav>
+  );
+}
+export default NavBar;
