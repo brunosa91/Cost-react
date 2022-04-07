c3af83819beb86f6f52095efa01e4f7b3aaf51c8
brunosa91 <brunosantana.engenharia@gmail.com>
2022-04-07 16:21:28 -0300

    Realizado requisição para api

--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -1,23 +1,48 @@
-function ProjectForm() {
-  return (
-    <form>
-      <div>
-        <input type="text" placeholder="Insira o nome do projeto" />
-      </div>
-      <div>
-        <input type="number" placeholder="Insira o orçamento total" />
-      </div>
+import styles from "./ProjectForm.module.css";
+import Input from "../form/Input";
+import Select from "../form/Select";
+import SubmitButton from "../form/SubmitButton";
+import { useState, useEffect } from "react";
+function ProjectForm({ btnText }) {
+  const [categories, setCategories] = useState([]);
 
-      <div>
-        <select name="category_id">
-          <option disabled selected>
-            Selecione a categoria
-          </option>
-        </select>
-      </div>
-      <div>
-        <input type="submit" value="Criar projeto" />
-      </div>
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <form className={styles.form}>
+      <Input
+        name="name"
+        text="Nome do projeto"
+        type="text"
+        placeholder="Insira o nome do projeto"
+      />
+
+      <Input
+        type="number"
+        text="Orçamento do projeto"
+        placeholder="Insira o orçamento total"
+        name="budget"
+      />
+
+      <Select
+        name="category_id"
+        text="Selecione a categoria"
+        options={categories}
+      />
+
+      <SubmitButton text={btnText} />
     </form>
   );
 }
